d34b3a51744ba63888a9d28cae4877d27aef4647
Miguel Gordian <os.aioria@gmail.com>
2022-10-04 01:24:15 -0500

    Add support for bubble charts

--- alud/src/App.js
+++ alud/src/App.js
@@ -6,10 +6,28 @@ import 'graphiql/graphiql.min.css';
 import GraphiQLExplorer from 'graphiql-explorer';
 
 import TextareaAutosize from '@mui/base/TextareaAutosize';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import SaveIcon from '@mui/icons-material/Save';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+
+import MDEditor from '@uiw/react-md-editor';
+import rehypeSanitize from "rehype-sanitize";
 
 import ReactJson from 'react-json-view'
+
+import ChartSelector from './components/chart-selector.js';
 
 const GRAPHQL_ENDPOINT = 'https://dex.dipdup.net/v1/graphql'; // 'https://swapi-graphql.netlify.app/.netlify/functions/index',
 
@@ -30,6 +48,30 @@ const fetcher = async graphQLParams => {
   return data.json().catch(()=> data.text());
 }
 
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index ? 
+          children
+      : <></>}
+    </div>
+  );
+}
 
 const App = () => {
   const [schema, setSchema] = useState(null);
@@ -38,9 +80,17 @@ const App = () => {
   const [response, setResponse] = useState({});
   const [filterQuery, setFilterQuery] = useState('');	
   const [filteredData, setFilteredData] = useState({});
-
+  const [chartType, setChartType] = useState('');
+  const [config, setConfig] = useState({responsive: true})
+  const [story, setStory] = useState("# Awesome Tezos Story");
+  const [value, setValue] = useState(0);
+ 
   const graphiQLRef = useRef(null);
-  
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   useEffect(() => {
     fetcher({
       query: getIntrospectionQuery()
@@ -72,34 +122,102 @@ const App = () => {
 
     return graphQLResponse;
   }
+  
+  const handleChartTypeChange = (event) => {
+    setChartType(event.target.value);
+  };
 
-  return (<div className='container'>
-    <GraphiQLExplorer
-      schema={schema}
-      query={query}
-      onEdit={setQuery}
-      onRunOperation={operationName =>
-        graphiQLRef.current ? graphiQLRef.handleRunQuery(operationName) : console.log(graphiQLRef)
-      }
-      explorerIsOpen={explorerIsOpen}
-      onToggleExplorer={() => setExplorerIsOpen(!explorerIsOpen) }
-    />
+  return (<Box>
+    <AppBar className="gutter" position="static" color="primary">
+      <Toolbar style={{ padding: 0 }}>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Alud
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+    <Box className="gutter"  sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Tabs value={value} onChange={handleChange} aria-label="Build Graphics">
+          <Tab style={{flexGrow: 1}} label="Query & Filter" {...a11yProps(0)} />
+          <Tab style={{flexGrow: 1}} label="Write a Story" {...a11yProps(1)} />
+          <Tab style={{flexGrow: 1}} label="Configure Chart" {...a11yProps(2)} />
+        <Button variant="outlined" startIcon={<SaveIcon />}>
+          Save
+        </Button>
+      </Tabs>
+    </Box>
 
-    <div style={{height: '100%', width: '100%'}}>
-      <GraphiQL
-        ref={ () =>  graphiQLRef}
-        fetcher={catchResponseFromFetcher}
-        schema={schema}
-        query={query}
-        onEditQuery={setQuery}
-        docExplorerOpen={false}
-      />
-      <Box style={{ 'display': 'grid', 'grid-template-columns': '64px [first] 47% 20px [end] 40%' }}>
-        <TextareaAutosize aria-label="Transformation query" placeholder="JSONata query" style={{ 'grid-column-start': 'first', 'height': 200 }} value={filterQuery} onChange={(event) => setFilterQuery(event.target.value)} />
-	<Paper style={{'grid-column-start': 'end'}}><ReactJson src={filteredData} /></Paper>
-      </Box>
-    </div>
-  </div>)
+    <TabPanel value={value} index={0}>
+      <div className='gutter container'>
+        <GraphiQLExplorer
+          schema={schema}
+          query={query}
+          onEdit={setQuery}
+          onRunOperation={operationName =>
+            graphiQLRef.current ? graphiQLRef.handleRunQuery(operationName) : console.log(graphiQLRef)
+          }
+          explorerIsOpen={explorerIsOpen}
+          onToggleExplorer={() => setExplorerIsOpen(!explorerIsOpen) }
+        />
+        <div style={{height: '100%', width: '100%'}}>
+          <GraphiQL
+            ref={ () =>  graphiQLRef}
+            fetcher={catchResponseFromFetcher}
+            schema={schema}
+            query={query}
+            onEditQuery={setQuery}
+            docExplorerOpen={false}
+          />
+          <Box style={{ 'display': 'grid', 'grid-template-columns': '64px [first] 46% 20px [end] auto' }}>
+            <TextareaAutosize aria-label="Transformation query" placeholder="JSONata query" style={{ 'grid-column-start': 'first', 'height': 200 }} value={filterQuery} onChange={(event) => setFilterQuery(event.target.value)} />
+	    <Paper style={{'grid-column-start': 'end'}}><ReactJson src={filteredData} /></Paper>
+          </Box>
+        </div>
+      </div>
+    </TabPanel>
+
+    <TabPanel value={value} index={1}>
+      <div className='gutter container'>
+        <div style={{height: '100%', width: '100%', marginTop: '26px'}}>
+          <MDEditor
+            value={story}
+            onChange={setStory}
+            previewOptions={{
+              rehypePlugins: [[rehypeSanitize]],
+            }}
+          />
+        </div>
+      </div>
+    </TabPanel>
+
+    <TabPanel value={value} index={2}>
+      <div className='gutter container'>
+        <div style={{height: '100%', width: '100%'}}>
+          <Box style={{ }}>
+            <FormControl style={{ marginTop: '26px' }}>
+            <InputLabel id="chart-type-selector">Select Chart Type</InputLabel>
+            <Select
+	      labelId="chart-type-selector"
+	      id="chart-type-select"
+	      value={chartType}
+	      label="Age"
+	      onChange={handleChartTypeChange}
+	      >
+	        <MenuItem value=''>None</MenuItem>
+	        <MenuItem value='bar'>Bar Chart</MenuItem>
+	        <MenuItem value='bubble'>Bubble Chart</MenuItem>
+	        <MenuItem value='pie'>Pie Chart</MenuItem>
+	        <MenuItem value='line'>Line Chart</MenuItem>
+	        <MenuItem value='radar'>Radar Chart</MenuItem>
+	        <MenuItem value='scatter'>Scatter Chart</MenuItem>
+	    </Select>
+            </FormControl>
+	    <ChartSelector chartType={chartType} data={filteredData} config={config} setConfig={setConfig} />
+          </Box>
+        </div>
+      </div>
+    </TabPanel>
+  </Box>)
 };
 
 export default App;
---
+++ alud/src/components/chart-selector.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import ReactJson from 'react-json-view'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Bubble } from 'react-chartjs-2';
+
+ChartJS.register(
+ CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+const ConfigEditor = props => {
+  const {config, setConfig} = props;
+
+  const updateConfigChange = obj => setConfig(obj.updated_src)
+
+  return <>
+    <ReactJson src={config} onAdd={updateConfigChange} onEdit={updateConfigChange} onDelete={updateConfigChange} style={props.style}/>
+  </>
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    this.props.setHasError(true)
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.props.hasError) {
+      // You can render any custom fallback UI
+      return <></>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+const ChartSelector = (props) => {
+  const {chartType, data, config, setConfig} = props;
+  const [hasError, setHasError] = useState(false);
+  let Chart;
+
+  useEffect(()=> {
+    setHasError(false);
+  }, [chartType, data, config])
+
+  if (chartType == 'bubble') {
+    try {
+      Chart = <ErrorBoundary hasError={hasError} setHasError={setHasError}>
+       <div
+          style={{ 'grid-column-start': 'first'  }} 
+       > 
+       <Bubble
+	  options={config}
+	  data={data}
+	/>
+      </div>
+      </ErrorBoundary>;
+    } catch (e) {
+      console.log("Failed generating chart");
+      console.log(e);
+    }
+    return <div style={{display: 'grid', 'grid-template-columns': '64px [first] 47% 20px [end] 40%'}}>
+      {Chart}
+      <ConfigEditor style={{ 'grid-column-start': 'end' }} config={config} setConfig={setConfig} />
+    </div>;
+  }
+
+  return <></>;
+
+}
+
+export default ChartSelector;
